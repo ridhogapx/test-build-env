6de76685c83932c8c9efed5a77dbd45047fbc7f6
RageNeko26 <rneko2006@gmail.com>
2023-09-16 15:17:39 +0700

    feat: add controller for handling SaveBook

--- controller/book_controller.go
+++ controller/book_controller.go
@@ -1,6 +1,27 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"test-build-env/config"
+	"test-build-env/model"
 
-func SaveBook(ctx *gin.Context) { 
+	"github.com/gin-gonic/gin"
+)
+
+func SaveBook(ctx *gin.Context) {
+  var book model.SaveBookRequest
+  
+  ctx.ShouldBindJSON(&book)
+
+  newData := model.Book{
+    Title: book.Title,
+    Author: book.Author,
+  }
+
+  config.DB.Create(&newData)
+
+  ctx.JSON(201, model.WebResponse{
+    Status: "success",
+    Message: "Book was saved!",
+  })
+
 }
